bd5a3d02d40f3547ba83402bd0a85a1ecfe5a609
NazBen <nazih.benoumechiara@gmail.com>
2017-06-06 16:00:58 +0200

    Cleanning

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -1,5 +1,5 @@
+#/usr/bin/env sh
 # Inspired from https://github.com/NazBen/scikit-optimize/blob/master/build_tools/travis/install.sh
-
 deactivate
 
 # Install conda using miniconda
@@ -10,7 +10,6 @@ cd download
 echo "Cached in $HOME/download :"
 ls -l
 echo
-
 if [[ ! -f miniconda.sh ]]; then
 	if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
 		wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
@@ -18,7 +17,6 @@ if [[ ! -f miniconda.sh ]]; then
 		wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
 	fi
 fi
-
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 conda update --yes conda
@@ -26,17 +24,16 @@ conda info -a
 popd
 
 # Create a conda env and install packages
-conda create -n testenv --yes python=$TRAVIS_PYTHON_VERSION pip nose numpy \
-	scipy scikit-learn matplotlib pandas h5py scikit-learn
+conda create -n testenv --yes python=$TRAVIS_PYTHON_VERSION numpy \
+	scipy scikit-learn matplotlib pandas h5py scikit-learn rpy2
 source activate testenv
 
 conda install --yes -c conda-forge openturns
-conda install --yes -c R R r-copula
 
 echo "Installing R package(s): $@"
 R -e 'install.packages("VineCopula", repos="https://cloud.r-project.org")'
 
-pip install pyDOE scikit-optimize rpy2
+pip install pyDOE scikit-optimize
 
 python --version
 python -c "import numpy; print('numpy %s' % numpy.__version__)"
