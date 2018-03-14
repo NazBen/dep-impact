3f892cfed91d2b12287a80306dac38cc31e0d9bc
NazBen <nazih.benoumechiara@gmail.com>
2018-03-14 20:06:47 +0100

    Cleaning and changed library name

--- setup.py
+++ setup.py
@@ -1,38 +1,22 @@
 ﻿#!/usr/bin/env python
-"""An OpenTURNS module for conservative estimation of risk quantities for
-incomplete dependence structure.
 
-See:
-https://github.com/NazBen/impact-of-dependence
-"""
-
-# Always prefer setuptools over distutils
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
-    
-from codecs import open
-from os import path
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
-# Get the long description from the README file
-here = path.abspath(path.dirname(__file__))
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
-    name='impact-of-dependence',
-    version='0.3',
-    description='A Python package to compute the impact of dependencies in \
-        probabilistic studies.',
-    url='https://github.com/NazBen/impact-of-dependence',
+    name='dep-impact',
+    version='0.1',
+    description='A Python package on the influence of dependencies in a structural reliability problem.',
+    url='https://github.com/NazBen/dep-impact',
     author='Nazih Benoumechiara',
     author_email='nazih.benoumechiara@gmail.com',
     license='MIT',
-    keywords='copula reliability openturns',
+    keywords='copula reliability regular-vines',
     packages=['dependence'],
     install_requires=required
 )
